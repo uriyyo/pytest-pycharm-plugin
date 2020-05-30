cd58f95732543ebcd1e04350929e5be017bf8cbc
Yurii Karabas <1998uriyyo@gmail.com>
2020-05-30 16:29:51 +0300

    🐛 Fix bugs with request introspection

--- src/main/java/com/uriyyo/pytest/fixtures/PyTestFixtureRequestCompletionContributor.kt
+++ src/main/java/com/uriyyo/pytest/fixtures/PyTestFixtureRequestCompletionContributor.kt
@@ -1,19 +1,18 @@
 package com.uriyyo.pytest.fixtures
 
-
 import com.jetbrains.python.psi.PyFunction
 import com.uriyyo.pytest.base.PyTestArgumentCompletion
 import com.uriyyo.pytest.base.PyTestParameterCompletionContributor
 import com.uriyyo.pytest.extensions.isFixture
+import com.uriyyo.pytest.extensions.isTest
 
 class PyTestFixtureRequestCompletionContributor : PyTestParameterCompletionContributor() {
 
     override fun getCompletion() = object : PyTestArgumentCompletion() {
         override fun getCompletions(pyFunction: PyFunction): Sequence<String>? =
-                if (pyFunction.isFixture())
+                if (pyFunction.isFixture || pyFunction.isTest)
                     sequenceOf("request")
                 else
                     emptySequence()
     }
-
 }
